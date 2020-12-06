12671ceff891753cfe652d1dcade6cc7c5306592
Ashvin Srivastava <59316380+ashvinsri@users.noreply.github.com>
2020-12-06 12:09:25 +0530

    Create Test.java

---
+++ src/LA1_SetInterface/Test.java
@@ -0,0 +1,38 @@
+/*
+
+Set Interface extends Collection Interface.
+Set:---->If u do not want an insertion order and want no duplicate then use Set 
+
+All 12 Collection interface methods are present in the Set Interface..
+
+
+
+                            Collection
+                                |
+                                |
+                               Set
+                        |                |
+                        |                |
+                     HashSet          Sorted Set
+                        |                |
+                        |                |
+                    LinkedHashSet     Navigable Set
+                                         |
+                                         |
+                                      Tree Set
+
+Set Interface doesn't have any new method
+
+HashSet:-  1)-->underlying data structure is Hash Table   2)--> Duplicates are not allowed   3)--> null insertion allowed but only ones   4)--> Hetrogenuus object allowed
+           5)-->Inserted based on Hash Code       6)- Best choice for search operation
+
+
+
+
+
+
+
+
+
+
+*/
